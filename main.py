755abfae31400cc71258d0a67ffd5902e915333e
Nikita Konzhin <83888910+KnowyX@users.noreply.github.com>
2025-02-03 21:48:08 +0300

    beta 1.2
    -added function for first button

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
+﻿#!/usr/bin/env python
+#!/usr/bin/env python3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets, QtCore
+import sys
+from mainUI import Ui_MainWindow
+from PyQt5.QtGui import QFont, QFontDatabase
+from ui2 import timetable
 
-app = QApplication([])
+window = None
+timetableW = None
 
-# Загружаем файл .ui
-window = uic.loadUi("main.ui")
+class mainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(mainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-window.show()
-app.exec_()
+
+    def hide(self):
+        self.close()
+
+
+class timetableWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(timetableWindow, self).__init__()
+        self.ui = timetable()
+        self.ui.setupUi(self)
+   
+def runSecWindow(info):
+   global timetableW
+   if info == 'Расписание':
+       timetableW = timetableWindow()
+       # window.hide()
+       timetableW.show()
+
+       
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = mainWindow()
+    # window.getUI()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
