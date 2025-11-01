404471ff3b4f1440c345e3ec5e23322b8ba5aea8
knowyx <knowyx@gmail.com>
2025-11-01 11:23:59 +0300

    добавление к v.2.2. работы с csv

--- resources/csvWorker.py
+++ resources/csvWorker.py
@@ -3,7 +3,7 @@ import csv
 
 def read(file):
     data = []
-    with open(file) as f:
+    with open(file, encoding='UTF-8') as f:
         reader = csv.DictReader(f)
         for x in reader:
             data.append(x)
